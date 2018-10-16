0b71a1b81ec4f8aa75774ab05e9565724c7548e6
Paulius Kazokas <BTpauliuskazokas@gmail.com>
2018-10-16 13:08:00 +0300

    optimized queries

--- src/main/resources/db/migration/V6__insertOrderTemplateValues.sql
+++ src/main/resources/db/migration/V6__insertOrderTemplateValues.sql
@@ -1,21 +1,14 @@
 INSERT INTO user.order_template VALUES (
+null, 
 null, null, 
 null, null,
-null,
 "lietuviu kalba", "persiku kalba",
 null, null,
 null
 );
 
-UPDATE user.order_template ot, user.user_data ud
-  SET ot.users_id = ud.id;
-  
-UPDATE user.order_template ot, user.order_data o
-  SET ot.orders_id = o.orders_id;
-  
-UPDATE user.order_template ot, user.order_data o, user.user_data u
-  SET ot.user_username = u.username;
-  
-UPDATE user.order_template ot
-  SET ot.order_date = CURRENT_DATE();
-  
+UPDATE user.order_template ot, user.user_data u, user.order_data o
+  SET ot.users_id = u.id,
+      ot.orders_id = o.orders_id,
+      ot.user_username = u.username,
+      ot.order_date = CURRENT_DATE();
